cb75bdacce300ca8eb70d0231a138f73fa8e33dd
Khalid Grandi <kh.grandi@gmail.com>
2018-05-01 15:31:01 +0100

    easyshell buttons list

--- namlat/modules/easy_shell.py
+++ namlat/modules/easy_shell.py
@@ -15,6 +15,14 @@ module_db = context.localdb['modules'][__name__]
 module_config = context.config['modules'][__name__]
 
 
+def _get_button(button_name):
+    if 'buttons' in module_config:
+        for button in module_config['buttons']:
+            if button['name'] == button_name:
+                return button
+    return None
+
+
 def mail_hook():
     pass
 
@@ -27,15 +35,16 @@ def get_flask_rules():
 def easy_shell_index():
     buttons = list()
     if 'buttons' in module_config:
-        for button in module_config['buttons'].values():
+        for button in module_config['buttons']:
             buttons.append(button['name'])
 
     return render_template("easy_shell/list_buttons.html", title="EasyShell Buttons", buttons=buttons)
 
 
-@flask_rule_container.route('/buttons/<button>')
-def button_view(button):
-    return_code, output = run_command_ex1(module_config['buttons'][button]['cmd_vector'])
-    return render_template("easy_shell/button_view.html", title="EasyShell: " +button, button=button,
+@flask_rule_container.route('/buttons/<button_name>')
+def button_view(button_name):
+    button = _get_button(button_name)
+    return_code, output = run_command_ex1(button['cmd_vector'])
+    return render_template("easy_shell/button_view.html", title="EasyShell: " +button_name, button=button_name,
                            output=output.decode(), return_code=return_code)
 
